f89a7127d6faa79b14fa9e975029f8c9ca4c5b70
Rodolfo <rodolfoafl@gmail.com>
2020-09-01 20:29:54 -0300

    Configured: TypeScript, ESLint, Prettier, git hook (Husky, Lint-Staged), Jest & RTL

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,21 +1,23 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
 
-export default function Home() {
+interface HomeProps {
+  title: string;
+}
+
+const Home: React.FC<HomeProps> = ({ title = "React Avançado" }) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -56,10 +58,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
