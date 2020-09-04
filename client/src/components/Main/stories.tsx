b22534cc89a05feddb77c719b5b1244be7f934ee
Rodolfo <rodolfoafl@gmail.com>
2020-09-04 01:30:11 -0300

    Installed and configured Storybook

---
+++ client/src/components/Main/stories.tsx
@@ -0,0 +1,14 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+
+import Main from ".";
+
+export default {
+  title: "Main",
+  component: Main,
+  args: {
+    title: "Default Title",
+    description: "Default Description"
+  }
+} as Meta;
+
+export const Basic: Story = (args) => <Main {...args} />;
